66c895cabb9a77f3b9d55c39c5d69856d268aa17
galoss88 <gallogarcianicolas@gmail.com>
2024-01-27 19:47:41 -0300

    avanceS

---
+++ src/hooks/useImageSlider.tsx
@@ -0,0 +1,138 @@
+import React, { useEffect, useState } from "react";
+import {
+  Typography,
+  IconButton,
+  Card,
+  CardMedia,
+  CardContent,
+  Box,
+} from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+interface Image {
+  title: string;
+  imageUrl: string;
+  price: string;
+}
+
+interface ImageSliderProps {
+  visibleImages: Image[];
+  nextSlide: () => void;
+  prevSlide: () => void;
+  SliderImages: () => React.ReactNode; // Corregir la definición de ComponenteSlider
+}
+
+const useImageSlider = (images: Image[]): ImageSliderProps => {
+  const [startIndex, setStartIndex] = useState<number>(0);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 600);
+  const visibleCount = isSmallScreen ? 1 : 3;
+  const visibleImages = images.slice(startIndex, startIndex + visibleCount);
+
+  const handleResize = () => {
+    setIsSmallScreen(window.innerWidth <= 600);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const nextSlide = () => {
+    setStartIndex((prevIndex) =>
+      Math.min(prevIndex + 1, images.length - visibleCount)
+    );
+  };
+
+  const prevSlide = () => {
+    setStartIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+  };
+
+  const SliderImages = () => {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "1rem",
+          flexDirection: isSmallScreen ? "column" : "row",
+          flexWrap: "wrap",
+          width: "100%",
+          maxWidth: "1200px",
+          margin: "0 auto",
+          backgroundColor: "red",
+        }}
+      >
+        {visibleImages.map((image, index) => (
+          <Card
+            key={index}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              flexGrow: 1,
+              flexShrink: 0,
+              flexBasis: isSmallScreen ? "100%" : "16rem",
+              maxWidth: isSmallScreen ? "100%" : "300px",
+              margin: "1rem",
+            }}
+          >
+            <CardMedia
+              component="img"
+              height="140"
+              alt={image.title}
+              image={image.imageUrl}
+            />
+            <CardContent>
+              <Typography variant="h5" gutterBottom>
+                {image.title}
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                Precio: {image.price}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+        {(isSmallScreen || visibleImages.length > 1) && (
+          <>
+            <IconButton
+              onClick={prevSlide}
+              style={{
+                position: "absolute",
+                left: 0,
+                top: "50%",
+                transform: "translateY(-50%)",
+                display: "block",
+              }}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+            <IconButton
+              onClick={nextSlide}
+              style={{
+                position: "absolute",
+                right: 0,
+                top: "50%",
+                transform: "translateY(-50%)",
+                display: "block",
+              }}
+            >
+              <ArrowForwardIcon />
+            </IconButton>
+          </>
+        )}
+      </Box>
+    );
+  };
+
+  return {
+    visibleImages,
+    nextSlide,
+    prevSlide,
+     SliderImages,
+  };
+};
+
+export default useImageSlider;
--- src/pages/home/components/recomendations/Recomendations.tsx
+++ src/pages/home/components/recomendations/Recomendations.tsx
@@ -1,16 +1,5 @@
-import React, { useEffect, useState } from "react";
-import {
-  Container,
-  Typography,
-  IconButton,
-  Card,
-  CardMedia,
-  CardContent,
-  Box,
-} from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import useImageSlider from "../../../../hooks/useImageSlider";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const Recomendations = () => {
   const images = [
@@ -51,104 +40,15 @@ const Recomendations = () => {
       price: "$35.00",
     },
   ];
-  const [isSmallScreen, setIsSmallScreen] = useState(
-    window.innerWidth <= 600
-  );
 
-  const handleResize = () => {
-    setIsSmallScreen(window.innerWidth <= 600);
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const { visibleImages, nextSlide, prevSlide } = useImageSlider(
-    images,
-    isSmallScreen ? 1 : 3
-  );
+  const { SliderImages } = useImageSlider(images);
 
   return (
     <Container style={{ position: "relative", textAlign: "center" }}>
       <Typography variant="h4" gutterBottom>
         Tus Recomendaciones
       </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "1rem", // Ajusta el espacio entre las tarjetas
-          flexDirection: isSmallScreen ? "column" : "row", // Cambia a columna en pantallas pequeñas
-          flexWrap: "wrap", // Permite que las tarjetas se envuelvan en pantallas pequeñas
-          width: "100%", // Ancho al 100% del contenedor padre
-          maxWidth: "1200px", // Ancho máximo de 1200px
-          margin: "0 auto", // Centrar el contenedor
-          backgroundColor: "red",
-        }}
-      >
-        {visibleImages.map((image, index) => (
-          <Card
-            key={index}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              textAlign: "center",
-              flexGrow: 1,
-              flexShrink: 0,
-              flexBasis: isSmallScreen ? "100%" : "16rem", // Ajusta el ancho en pantallas pequeñas
-              maxWidth: isSmallScreen ? "100%" : "300px", // Ajusta el ancho máximo en pantallas pequeñas
-              margin: "1rem", // Ajusta el margen entre las tarjetas
-            }}
-          >
-            <CardMedia
-              component="img"
-              height="140"
-              alt={image.title}
-              image={image.imageUrl}
-            />
-            <CardContent>
-              <Typography variant="h5" gutterBottom>
-                {image.title}
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                Precio: {image.price}
-              </Typography>
-            </CardContent>
-          </Card>
-        ))}
-      </Box>
-
-      {(isSmallScreen || visibleImages.length > 1) && (
-        <>
-          <IconButton
-            onClick={prevSlide}
-            style={{
-              position: "absolute",
-              left: 0,
-              top: "50%",
-              transform: "translateY(-50%)",
-              display: "block",
-            }}
-          >
-            <ArrowBackIcon />
-          </IconButton>
-          <IconButton
-            onClick={nextSlide}
-            style={{
-              position: "absolute",
-              right: 0,
-              top: "50%",
-              transform: "translateY(-50%)",
-              display: "block",
-            }}
-          >
-            <ArrowForwardIcon />
-          </IconButton>
-        </>
-      )}
+      <SliderImages />
     </Container>
   );
 };
